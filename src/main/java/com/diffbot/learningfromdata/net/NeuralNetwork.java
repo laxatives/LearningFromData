4666a9b1b1179b8fd02abc6391a2983f27227bb9
Benjamin Han <bhan@connectifier.com>
2016-05-10 16:03:36 -0700

    Start implementing backprop

--- src/main/java/com/diffbot/learningfromdata/net/NeuralNetwork.java
+++ src/main/java/com/diffbot/learningfromdata/net/NeuralNetwork.java
@@ -18,11 +18,20 @@ public class NeuralNetwork {
 			public double eval(double x) {
 				return Math.tanh(x);
 			}
+			@Override
+			public double derivative(double x) {
+				return 1 - Math.pow(Math.tanh(x), 2);
+			}
 		},
 		RELU {
 			@Override
 			public double eval(double x) {
 				return Math.max(0, x);
+			}
+
+			@Override
+			public double derivative(double x) {
+				return x < 0 ? 0 : 1;
 			}
 		},
 		LEAKY_RELU {
@@ -31,14 +40,22 @@ public class NeuralNetwork {
 			public double eval(double x) {
 				return x < 0 ? a * x : x; 
 			}
+			@Override
+			public double derivative(double x) {
+				return x < 0 ? a : 1;
+			}
 		};
 		public abstract double eval(double x);
+		public abstract double derivative(double x);
 	}
 	
 	public NeuralNetwork(int[] shape) {
 		this(shape, ActivationFunction.RELU);
 	}
 	
+	/**
+	 * First layer size must be equal to number of features.
+	 */
 	public NeuralNetwork(int[] shape, ActivationFunction f) {
 		this.f = f;
 		int inputSize = 1;
@@ -54,7 +71,7 @@ public class NeuralNetwork {
 		}
 	}
 	
-	public int forward(double[] x) {
+	public double[] eval(double[] x) {
 		double[] input = x;
 		for (int i = 0; i < neurons.size(); i++) {
 			List<Neuron> layer = neurons.get(i);			
@@ -67,24 +84,53 @@ public class NeuralNetwork {
 			input = output;
 		}
 		
-		int result = 0;
-		double maxWeight = Double.MIN_VALUE;
-		for (int i = 0; i < input.length; i++) {
-			if (input[i] > maxWeight) {
-				result = i;
-				maxWeight = input[i];
-			}
+		return input;
+	}
+	
+	public void train(double[][] xs, double[] ys) {
+		for (int i = 0; i < xs.length; i++) {
+			// TODO: use minibatch
+			double[] x = xs[i];
+			double y = ys[i];
+			double[] guess = eval(x);	
+			
+			backprop(guess, y);
 		}
-		
-		return result;
 	}
 	
-	public static int backward() {
-		// TODO
-		return 0;
+	private void backprop(double[] guess, double y) {
+		double[] output = guess;
+		double[] partialDerivatives;
+		for (int i = neurons.size() - 1; i >= 0; i--) {
+			List<Neuron> layer = neurons.get(i);
+
+			double[] input = new double[layer.size()];
+			
+			for (int j = 0; j < input.length; j++) {
+				Neuron neuron = layer.get(j);
+				double[] gradient = null; // need edge values from forward pass
+				input[j] = f.derivative(layer.get(j).dotProduct(output));
+			}
+			
+			output = input;
+		}
+	}
+	
+	// squared hinge loss
+	public static double loss(double[] output, double y) {
+		double loss = 0;
+		int correctIndex = (int) y;
+		for (int i = 0; i < output.length; i++) {
+			if (i == correctIndex) {
+				continue;
+			}
+			loss += Math.pow(Math.max(0, output[i] - y + 1), 2);
+		}
+		return loss;
 	}
 	
 	
+	private static final int MAX_ITERATIONS = 200;
 	private static final Double HOLDOUT_PERCENTAGE = 0.1;
 	
 	public static void main(String[] args) throws IOException {
@@ -95,11 +141,19 @@ public class NeuralNetwork {
 		// 2 hidden layers of input size
 		int[] shape = new int[]{MnistHandwrittenDigitData.NUM_FIELDS, MnistHandwrittenDigitData.NUM_FIELDS, MnistHandwrittenDigitData.NUM_FIELDS, 10};
 		NeuralNetwork nn = new NeuralNetwork(shape);
-		for (int i = 0; i < trainSet.xs.length; i++) {
-			double[] x = trainSet.xs[i];
-			double y = trainSet.ys[i];
-			nn.forward(x);
+		for (int i = 1; i <= MAX_ITERATIONS; i++) {
+			nn.train(trainSet.xs, trainSet.ys);			
 		}
+		
+		double loss = 0;
+		for (int i = 0; i < testSet.xs.length; i++) {
+			double[] x = testSet.xs[i];
+			double y = testSet.ys[i];
+			
+			double[] guess = nn.eval(x);
+			loss += loss(guess, y);
+		}
+		System.out.println("Average loss: " + loss / (float) testSet.xs.length);
 	}
 
 }
