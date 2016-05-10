b51c2c6fa2496289661ef1785dfb6e8854bb41a2
Benjamin Han <bhan@connectifier.com>
2016-05-10 16:03:36 -0700

    Start implementing backprop

--- src/main/java/com/diffbot/learningfromdata/net/Neuron.java
+++ src/main/java/com/diffbot/learningfromdata/net/Neuron.java
@@ -5,19 +5,32 @@ import java.util.Random;
 import com.diffbot.learningfromdata.utils.MathUtils;
 
 public class Neuron {	
+	private static final Double INIT_WEIGHT = 1e-3;
+	private static final double ETA = 0.1; // learning parameter
 	private static Random RANDOM = new Random();
 	
 	public double[] w;
+	public double val;
 	
+	/**
+	 * Init weights to small numbers drawn from gaussian, bias = 0.
+	 */
 	public Neuron(int numFeatures) {
 		w = new double[numFeatures + 1];
-		for (int i = 0; i < w.length; i++) {
-			w[i] = RANDOM.nextGaussian();
+		for (int i = 1; i < w.length; i++) {
+			w[i] = INIT_WEIGHT * RANDOM.nextGaussian();
 		}
 	}
 	
 	public double dotProduct(double[] x) {
-		return MathUtils.dotProduct(w, MathUtils.padBias(x));
+		val = MathUtils.dotProduct(w, MathUtils.padBias(x));
+		return val;
+	}
+	
+	public void updateW(double[] gradient) {
+		// TODO: support momentum, conjugate descent
+		gradient = MathUtils.scalarProduct(-ETA, gradient);
+		w = MathUtils.sumArrays(w, gradient);
 	}
 
 }
